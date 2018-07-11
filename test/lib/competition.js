7a91f2c0ecf8ad9ca7c8949f29f068eb577187e8
Daniel Caris <dcaris86@gmail.com>
2018-07-11 16:34:09 +1000

    Refactored code so that Competition model isn't so poluted. Add 'rounds' test

--- test/lib/competition.js
+++ test/lib/competition.js
@@ -3,7 +3,7 @@ const expect = chai.expect;
 const chaiAsPromised = require('chai-as-promised');
 const dirtyChai = require('dirty-chai');
 const sinon = require('sinon');
-const Competition = require('../../lib/model/competition');
+const Ludus = require('../../lib/ludus');
 
 chai.use(chaiAsPromised);
 chai.use(dirtyChai);
@@ -12,14 +12,30 @@ describe('the competition module', () => {
     var competitions;
     var nrl;
     before((() => {
-        competitions = Competition.getAllCompetitions();
+        competitions = Ludus.competitions();
     }));
     it('should list all competitions', () => {
         expect(competitions).not.empty();
     });
     it('should locate a competition by code', () => {
-        nrl = Competition.getCompetition('nrl');
+        nrl = Ludus.competition('nrl');
         expect(nrl).not.null();
         expect(nrl.code).equals('nrl');
+    });
+    it('should list all clubs for a competition', () => {
+        var teams = [];
+        nrl.clubs(result => {
+            teams = result;
+            expect(teams).not.empty();
+        });
+    });
+    it('should list all rounds for a competition for a given year', () => {
+        var rounds = [];
+        return Promise.resolve(nrl.rounds(new Date().getFullYear()))
+            .then(result => {
+                rounds = result;
+                expect(rounds).not.empty();
+            })
+            .catch((error) => { throw new Error('was not supposed to fail'); });
     });
 });
