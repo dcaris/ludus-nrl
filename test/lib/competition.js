bd12315e3c7f6c0b0e8a8cbbc893208f3716a79f
Daniel Caris <dcaris86@gmail.com>
2018-07-19 20:01:06 +1000

    Basic round match extraction working (game date), now to get scores and players

--- test/lib/competition.js
+++ test/lib/competition.js
@@ -2,17 +2,32 @@ const chai = require('chai');
 const expect = chai.expect;
 const chaiAsPromised = require('chai-as-promised');
 const dirtyChai = require('dirty-chai');
-const sinon = require('sinon');
 const Ludus = require('../../lib/ludus');
+const Competition = require('../../lib/model/competition');
+const NrlCompetition = require('../../lib/competitions/nrl');
+const Round = require('../../lib/model/round');
+const Team = require('../../lib/model/team');
+const Match = require('../../lib/model/match');
 
 chai.use(chaiAsPromised);
 chai.use(dirtyChai);
 
 describe('the competition module', () => {
+    /** @type {Competition[]} */
     var competitions;
+    /** @type {NrlCompetition} */
     var nrl;
+    /** @type {number} */
+    var year;
+    /** @type {Team[]} */
+    var teams;
+    /** @type {Round[]} */
+    var rounds;
+    /** @type {Match[]} */
+    var matches;
     before((() => {
         competitions = Ludus.competitions();
+        year = new Date().getFullYear();
     }));
     it('should list all competitions', () => {
         expect(competitions).not.empty();
@@ -23,19 +38,40 @@ describe('the competition module', () => {
         expect(nrl.code).equals('nrl');
     });
     it('should list all clubs for a competition', () => {
-        var teams = [];
+        expect(nrl).is.not.null();
         nrl.clubs(result => {
             teams = result;
             expect(teams).not.empty();
         });
     });
     it('should list all rounds for a competition for a given year', () => {
-        var rounds = [];
-        return Promise.resolve(nrl.rounds(new Date().getFullYear()))
+        expect(nrl).is.not.null();
+        return Promise.resolve(nrl.rounds(year))
             .then(result => {
                 rounds = result;
                 expect(rounds).not.empty();
-            })
-            .catch((error) => { throw new Error('was not supposed to fail'); });
+            });
+    });
+    it('should list all match results per round for a given year', () => {
+        expect(rounds).is.not.empty();
+        return Promise.resolve(nrl.results(rounds[0]))
+            .then(result => {
+                matches = result;
+                expect(matches).not.empty();
+                matches.forEach(g => {
+                    expect(g).is.not.null();
+                    expect(g.date).is.not.null();
+                    expect(g.date).is.not.NaN();
+                    expect(g.date.getFullYear()).eql(rounds[0].year);
+
+                    expect(g.homeTeam).is.not.null();
+                    expect(g.homeTeam.players).is.not.empty();
+                    expect(g.homeTeamScore).is.gte(0);
+
+                    expect(g.awayTeam).is.not.null();
+                    expect(g.awayTeam.players).is.not.empty();
+                    expect(g.awayTeamScore).is.gte(0);
+                });
+            });
     });
 });
