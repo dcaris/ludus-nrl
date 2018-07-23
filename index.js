d8084422531be2fab559965e2c9705ecc8bc04d9
Daniel Caris <dcaris86@gmail.com>
2018-07-23 15:44:41 +1000

    ESlint - so i dont get errors when using async await

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 #!/usr/bin/env node
 
-const chalk = require("chalk");
+/*eslint-disable no-unused-vars*/
+
+const chalk = require('chalk');
 const clear = require('clear');
 const figlet = require('figlet');
 const program = require('commander');
--- test/lib/competition.js
+++ test/lib/competition.js
@@ -1,3 +1,4 @@
+/*eslint-disable no-unused-vars*/
 const chai = require('chai');
 const expect = chai.expect;
 const chaiAsPromised = require('chai-as-promised');
@@ -71,7 +72,34 @@ describe('the competition module', () => {
                     expect(g.date, 'Match Date').is.not.NaN();
                     expect(g.date.getFullYear(), 'Match Date Year').eql(rounds[0].year);
                     expect(g.date.getHours(), 'Match Game Time').is.gt(0);
-                    expect(g.venue, "Match Venue").is.not.empty();
+                    expect(g.venue, 'Match Venue').is.not.empty();
+
+                    // Validate home and away team details
+                    expect(g.homeTeam, 'Home Team').is.not.null();
+                    expect(g.homeTeam.club.nickname, 'Home Team Name').is.not.empty();
+                    expect(g.awayTeam, 'Away Team').is.not.null();
+                    expect(g.awayTeam.club.nickname, 'Away Team Name').is.not.empty();
+
+                    // Validate home and away team score
+                    expect(g.homeTeam.score, 'Home Team Score').is.gte(0);
+                    expect(g.awayTeam.score, 'Away Team Score').is.gte(0);
+                });
+            });
+    });
+    it('should list detailed match results per round for a given year', () => {
+        expect(rounds).is.not.empty();
+        return Promise.resolve(nrl.results(rounds[0], true))
+            .then(result => {
+                matches = result;
+                expect(matches).not.empty();
+                matches.forEach(g => {
+                    // Validate match details
+                    expect(g, 'Match').is.not.null();
+                    expect(g.date, 'Match Date').is.not.null();
+                    expect(g.date, 'Match Date').is.not.NaN();
+                    expect(g.date.getFullYear(), 'Match Date Year').eql(rounds[0].year);
+                    expect(g.date.getHours(), 'Match Game Time').is.gt(0);
+                    expect(g.venue, 'Match Venue').is.not.empty();
 
                     // Validate home and away team details
                     expect(g.homeTeam, 'Home Team').is.not.null();
