34095642e6787d0eb06bae6faa782d5490c4ec39
Daniel Caris <dcaris86@gmail.com>
2018-08-02 06:54:20 +1000

    Detailed player statistics now parsing for NRL mathces. WOOT!!!

--- test/lib/competition.js
+++ test/lib/competition.js
@@ -86,35 +86,43 @@ describe('the competition module', () => {
                 });
             });
     });
-    it('should list detailed match results per round for a given year', () => {
-        expect(rounds).is.not.empty();
-        return Promise.resolve(nrl.results(rounds[0], true))
+    it('should list detailed match results per round for a given year', function(done) {
+        //this.timeout(20000);
+        //setTimeout(done, 10000);
+
+        expect(matches).is.not.empty();
+        return Promise.resolve(nrl.match(rounds[0].year, rounds[0].number, matches[0].homeTeam.club.nickname, matches[0].awayTeam.club.nickname))
             .then(result => {
-                matches = result;
-                expect(matches).not.empty();
-                matches.forEach(g => {
-                    // Validate match details
-                    expect(g, 'Match').is.not.null();
-                    expect(g.date, 'Match Date').is.not.null();
-                    expect(g.date, 'Match Date').is.not.NaN();
-                    expect(g.date.getFullYear(), 'Match Date Year').eql(rounds[0].year);
-                    expect(g.date.getHours(), 'Match Game Time').is.gt(0);
-                    expect(g.venue, 'Match Venue').is.not.empty();
+                const detailedResult = result;
+                expect(detailedResult, 'Match').is.not.null();
+                expect(detailedResult.date, 'Match Date').is.not.null();
+                expect(detailedResult.date, 'Match Date').is.not.NaN();
+                expect(detailedResult.date.getFullYear(), 'Match Date Year').eql(rounds[0].year);
+                expect(detailedResult.date.getHours(), 'Match Game Time').is.gt(0);
+                expect(detailedResult.venue, 'Match Venue').is.not.empty();
+                expect(detailedResult.attendance, 'Match Attendance').is.gt(0);
 
-                    // Validate home and away team details
-                    expect(g.homeTeam, 'Home Team').is.not.null();
-                    expect(g.homeTeam.club.nickname, 'Home Team Name').is.not.empty();
-                    expect(g.awayTeam, 'Away Team').is.not.null();
-                    expect(g.awayTeam.club.nickname, 'Away Team Name').is.not.empty();
-
-                    // Validate home and away team score
-                    expect(g.homeTeam.score, 'Home Team Score').is.gte(0);
-                    expect(g.awayTeam.score, 'Away Team Score').is.gte(0);
-
-                    // Validate home and away squads
-                    expect(g.homeTeam.players, 'Home Team Players').is.not.empty();
-                    expect(g.awayTeam.players, 'Away Team Players').is.not.empty();
+                expect(detailedResult.awayTeam.players, 'Away Team Players').is.not.empty();
+                detailedResult.awayTeam.players.forEach(p => {
+                    expect(p.firstName, 'Away Team - Player Firstname').is.not.empty();
+                    expect(p.lastName, 'Away Team - Player Lastname').is.not.empty();
+                    expect(p.playerId, 'Away Team - Player Id').is.not.NaN();
+                    expect(p.position, 'Away Team - Player Position').is.not.empty();
+                    expect(p.positionNumber, 'Away Team - Player Position Number').is.gt(0);
+                    expect(p.matchStats, 'Away Team - Player Match Stats').is.not.null();
+                    expect(p.matchStats.minutesPlayed, 'Away Team - Player Match Stats - Mins Played').is.gt(0);
                 });
+
+                // expect(detailedResult.homeTeam.players, 'Home Team Players').is.not.empty();
+                // detailedResult.homeTeam.players.forEach(p => {
+                //     expect(p.firstName, 'Home Team - Player Firstname').is.not.empty();
+                //     expect(p.lastName, 'Home Team - Player Lastname').is.not.empty();
+                //     expect(p.playerId, 'Home Team - Player Id').is.not.NaN();
+                //     expect(p.position, 'Home Team - Player Position').is.not.empty();
+                //     expect(p.positionNumber, 'Home Team - Player Position Number').is.gt(0);
+                //     expect(p.matchStats, 'Home Team - Player Match Stats').is.not.null();
+                //     expect(p.matchStats.minutesPlayed, 'Home Team - Player Match Stats - Mins Played').is.gt(0);
+                // });
             });
     });
 });
